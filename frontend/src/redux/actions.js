6606b7eef02cfacdba24ef30415aadb1c80684fe
Kevin-Carlos <kevin_carlos@nevada.unr.edu>
2019-10-08 15:49:50 -0700

    Point location to cycle url

--- frontend/src/redux/actions.js
+++ frontend/src/redux/actions.js
@@ -39,7 +39,7 @@ export const incrementIndex = () => {
 export const fetchCards = () => {
     return (dispatch) => {
         dispatch(requestCardsLoading());
-        fetch("http://localhost:5000")
+        fetch("http://frontend-5d9d0d185176a800012b1e2e.c.5d8fa59da99b6b00011665f3.cycle.io/")
             .then(res => res.json())
             .then(json => dispatch(requestCardsSuccess(json)))
     }
@@ -47,7 +47,7 @@ export const fetchCards = () => {
 
 export const postCards = (data) => {
     return (dispatch) => {
-        const location = "http://localhost:5000/add";
+        const location = "http://frontend-5d9d0d185176a800012b1e2e.c.5d8fa59da99b6b00011665f3.cycle.io//add";
         const settings = {
             method: 'POST',
             body: JSON.stringify(data),
